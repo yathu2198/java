d3b06e345e74e64d5520390de2edb0037f539dcc
yathu2198 <yathu3584@gmail.com>
2021-11-09 23:12:36 +0530

    java

---
+++ Employee.java
@@ -0,0 +1,15 @@
+public class Employee {
+    int id;
+    String name;
+
+    void setEmployee(){
+        id=1000;
+        name="Yathu";
+    }
+
+    void getEmployee(){
+        System.out.println("Employee ID:"+id);
+        System.out.println("Employee Name:"+name);
+    }
+
+}
---
+++ MainEmployee.java
@@ -0,0 +1,13 @@
+public class MainEmployee{
+
+    public  static  void main(String args[]){
+        Employee emp;
+        emp =new Employee();
+
+
+        emp.setEmployee();
+        emp.getEmployee();
+
+    }
+
+}
